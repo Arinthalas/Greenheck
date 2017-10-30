2de5d67a9c5afaeaa3c0a526ed05a257ceff5e95
Arinthalas <32649470+Arinthalas@users.noreply.github.com>
2017-10-30 12:13:51 -0500

    Add files via upload

---
+++ Greenheck Project/Greenheck Project/User Interface/DetailsForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Greenheck_Project.User_Interface
+{
+    public partial class DetailsForm : Form
+    {
+        public int passedInfo;
+        public int passedYear;
+        public int passedQuarter;
+        public DetailsForm()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void DetailsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
